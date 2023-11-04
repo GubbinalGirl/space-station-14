a2de6a7bbfd689a77badf6ea784ca3d33cd9f221
Gubbinal Girl <GubbinalGirl@gmail.com>
2023-11-04 15:50:13 -0500

    Need to figure out fixtures.

--- Content.Server/BodyScanner/BodyScannerSystem.cs
+++ Content.Server/BodyScanner/BodyScannerSystem.cs
@@ -20,6 +20,7 @@ public sealed class BodyScannerSystem : SharedBodyScannerSystem
 
     private void OnCollide(Entity<BodyScannerComponent> bodyScanner, ref StartCollideEvent args)
     {
+        Log.Info("Bodyscanner OnCollide.");
         //Check if the other entity has a bodycomponent or inventorycomponent
         var other = args.OtherEntity;
 
@@ -47,12 +48,16 @@ public sealed class BodyScannerSystem : SharedBodyScannerSystem
     //AppearanceComponent is how the server communicates visualizer data to the client
     private void UpdateAppearance(Entity<BodyScannerComponent> bodyScanner)
     {
+        Log.Info("Updating Bodyscanner Appearance.");
         AppearanceComponent? appearance = null;
 
         //Do I need to try and get the AppearanceComponent?
         if (!Resolve(bodyScanner.Owner, ref appearance))
             return;
 
-        _appearance.SetData(bodyScanner.Owner, BodyScannerVisuals.Alerted, true, appearance);
+        if (bodyScanner.Comp.IsAlerted)
+            _appearance.SetData(bodyScanner.Owner, BodyScannerVisuals.Alerted, true, appearance);
+        else
+            _appearance.SetData(bodyScanner.Owner, BodyScannerVisuals.Base, true, appearance);
     }
 }
